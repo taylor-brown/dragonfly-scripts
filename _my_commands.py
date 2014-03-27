a0ef68ea15fd3e28feaab080dc790bbef0b03e03
Kevin Menard <kevin@nirvdrum.com>
2014-03-27 16:30:15 -0400

    More miscellaneous commands that I use.

--- _my_commands.py
+++ _my_commands.py
@@ -5,14 +5,39 @@ config.cmd = Section("helpers")
 config.cmd.map = Item(
     {
         "cd (mogo test|mogotest)": Text("cd ~/dev/workspaces/mogotest") + Key("enter"),
+        "cd (mogo test|mogotest) remote": Text("cd ~/dev/workspaces/mogotest-helix") + Key("enter"),
+        "cd ping4": Text("cd ~/dev/workspaces/ping4app") + Key("enter"),
+        "cd ping4 remote": Text("cd ~/ping4-helix") + Key("enter"),
         "cd workspaces": Text("cd ~/dev/workspaces") + Key("enter"),
         "cd home": Text("cd ~") + Key("enter"),
         "cd downloads": Text("cd ~/Downloads") + Key("enter"),
         "cd drop box": Text("cd ~/Dropbox") + Key("enter"),
+        "cd NatLink": Text("cd C:\NatLink\NatLink\MacroSystem") + Key("enter"),
 
         "(open|start) rails console": Text("./script/rails c") + Key("enter"),
+        "fix line endings": Text("find . -type f -exec dos2unix {} \; && chmod +x script/*") + Key("enter"),
+        "fix (date|time)": Text("ntpd -qg") + Key("enter"),
+        "stop torquebox": Text("service monit stop && service torquebox stop") + Key("enter"),
+        "start torquebox": Text("service torquebox start") + Key("enter"),
+        "grep torquebox": Text("ps aux | grep torque") + Key("enter"),
+
 
         "find URL group": Text("g = UrlGroup.where(uid: '').first") + Key("left:8"),
+
+        "ssh Q A gateway": Text("ssh qa-gw.ping4inc.com") + Key("enter"),
+        "ssh Q A mets": Text("ssh qa-mets.ping4inc.com") + Key("enter"),
+        "ssh load test gateway": Text("ssh loadtest-gw.ping4inc.com") + Key("enter"),
+        "ssh load test mets": Text("ssh loadtest-mets.ping4inc.com") + Key("enter"),
+        "ssh melchior": Text("ssh nirvdrum@melchior") + Key("enter"),
+        "ssh ping4 (development|V M)": Text("ssh development.ping4inc.com") + Key("enter"),
+        "ssh Kevin dot mogotest dot com": Text("ssh kevin.mogotest.com") + Key("enter"),
+
+        "tail app logs": Text("cd ~") + Key("enter") + Text("current") + Key("enter") + Text("tail -f log/*.log") + Key("enter"),
+        "tail (torquebox|torque box) log": Text("tail -f /var/log/torquebox/torquebox.log") + Key("enter"),
+        "tail development logs": Text("tail -f log.log"
+                                      "/development.log") + Key("enter"),
+
+        "(open|start) program": Key("a-space"),
 
         # Custom vocabulary.
         "nerve drum": Text("nirvdrum"),
@@ -32,7 +57,10 @@ config.cmd.map = Item(
 
         "ruby version": Text("ruby --version") + Key("enter"),
         "change ruby M R I": Text("rbenv shell 2.1.0") + Key("enter"),
-        "change (jay ruby|jruby) 1.7.10": Text("rbenv shell jruby-1.7.10") + Key("enter"),
+        "change ruby (jay ruby|jruby)": Text("rbenv shell jruby-1.7.10") + Key("enter"),
+
+        "(torque box|TorqueBox) run": Text("torquebox run -b 0.0.0.0") + Key("enter"),
+        "(torque box|TorqueBox) deploy": Text("torquebox deploy") + Key("enter"),
     }
 )
 
