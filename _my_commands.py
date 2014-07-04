35ebe7f936f7b942d180f3f12aaa45ba5e6329bf
Kevin Menard <kevin@nirvdrum.com>
2014-07-03 21:48:00 -0400

    Cleaning the slate as I prepare for a big Aenea upgrade.

--- _my_commands.py
+++ _my_commands.py
@@ -1,4 +1,4 @@
-from dragonfly import Config, Section, Item, MappingRule, Grammar, Dictation
+from dragonfly import Config, Section, Item, MappingRule, Grammar, Dictation, Pause, Function
 
 from lib.dynamic_aenea import (
     DynamicAction,
@@ -6,6 +6,8 @@ from lib.dynamic_aenea import (
     Key,
     Text
 )
+
+import lib.format
 
 config = Config("my commands")
 config.cmd = Section("helpers")
@@ -23,6 +25,11 @@ config.cmd.map = Item(
         "cd drop box": Text("cd ~/Dropbox") + Key("enter"),
         #"cd NatLink": Text("cd C:\NatLink\NatLink\MacroSystem") + Key("enter"),
         "cd NatLink": Text("cd /c/NatLink/NatLink/MacroSystem") + Key("enter"),
+        "cd rubber Ping4 production": Text("cd ~/.rubber_ping4_production") + Key("enter"),
+        "cd rubber Ping4 (development|staging)": Text("cd ~/.rubber_ping4_dev") + Key("enter"),
+        "cd rubber Mogotest": Text("cd ~/.ec2_mogotest") + Key("enter"),
+
+        "(edit|vim) rubber secret": Text("vim rubber-secret.yml"),
 
         "(open|start) rails console": Text("./script/rails c") + Key("enter"),
         "fix line endings": Text("find . -type f -exec dos2unix {} \; && chmod +x script/*") + Key("enter"),
@@ -31,9 +38,12 @@ config.cmd.map = Item(
         "start torquebox": Text("service torquebox start") + Key("enter"),
         "grep torquebox": Text("ps aux | grep torque") + Key("enter"),
 
+        "git get": Text("git get") + Key("enter"),
+        "git put": Text("git put") + Key("enter"),
 
         "find URL group": Text("g = UrlGroup.where(uid: '').first") + Key("left:8"),
 
+        "ssh mets": Text("ssh mets.ping4central.com") + Key("enter"),
         "ssh Q A gateway": Text("ssh qa-gw.ping4inc.com") + Key("enter"),
         "ssh Q A mets": Text("ssh qa-mets.ping4inc.com") + Key("enter"),
         "ssh load test gateway": Text("ssh loadtest-gw.ping4inc.com") + Key("enter"),
@@ -41,7 +51,6 @@ config.cmd.map = Item(
         "ssh melchior": Text("ssh nirvdrum@melchior") + Key("enter"),
         "ssh ping4 (development|V M)": Text("ssh development.ping4inc.com") + Key("enter"),
         "ssh Kevin dot mogotest dot com": Text("ssh kevin.mogotest.com") + Key("enter"),
-        "ssh melchior": Text("ssh melchior") + Key("enter"),
 
         "ssh app (oh|zero) 1": Text("ssh app01.mogotest.com") + Key("enter"),
         "ssh app (oh|zero) 2": Text("ssh app02.mogotest.com") + Key("enter"),
@@ -58,14 +67,17 @@ config.cmd.map = Item(
 
         "tail app logs": Text("cd ~") + Key("enter") + Text("current") + Key("enter") + Text("tail -f log/*.log") + Key("enter"),
         "tail (torquebox|torque box) log": Text("tail -f /var/log/torquebox/torquebox.log") + Key("enter"),
-        "tail development logs": Text("tail -f log.log/development.log") + Key("enter"),
+        "tail development logs": Text("tail -f log/development.log") + Key("enter"),
+        "tail postgres (log|logs)": Text("tail -f /var/log/postgresql/postgresql*.log") + Key("enter"),
 
         "edit ssh config": Text("vim ~/.ssh/config") + Key("enter"),
 
         "(open|start) program": DynamicAction(Key("a-space"), Key("f1")),
+        "(open|start) program <text>": DynamicAction(Key("a-space"), Key("f1")) + Pause("30") + Function(lib.format.lowercase_text),
 
         # Custom vocabulary.
         "nerve drum": Text("nirvdrum"),
+        "chat laugh": Text("Heh."),
         "J ruby": Text("jruby"),
         "you tills": Text("utils"),
         "firefox you tills": Text("FirefoxUtils"),
@@ -86,6 +98,12 @@ config.cmd.map = Item(
 
         "(torque box|TorqueBox) run": Text("torquebox run -b 0.0.0.0") + Key("enter"),
         "(torque box|TorqueBox) deploy": Text("torquebox deploy") + Key("enter"),
+
+        "vagrant destroy": Text("vagrant destroy"),
+        "vagrant up": Text("vagrant up"),
+        "vagrant up [with] virtual box": Text("vagrant up --provider=virtualbox"),
+        "vagrant up [with] VMware": Text("vagrant up --provider=vmware_workstation"),
+        "vagrant ssh": Text("vagrant ssh"),
     }
 )
 
